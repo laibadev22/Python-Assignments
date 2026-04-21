a6f1c544f958118e447d16b413235300977ce8d3
laibadev22 <laibadev723@gmail.com>
2026-04-20 23:36:44 -0700

    Add files via upload

--- 01_Task.py
+++ 01_Task.py
@@ -1,14 +1,21 @@
+a = input("Enter your Name: ")
 
-a= input("Enter your Name: ")
+def get_marks(subject):
+    while True:
+        try:
+            marks = int(input(f"Enter your Marks for {subject}: "))
+            return marks
+        except ValueError:
+            print("Invalid input! Please enter numbers only.")
 
-m1=int(input("Enter your Marks for subject1: "))
-m2=int(input("Enter your Marks for subject2: "))
-m3=int(input("Enter your Marks for subject3: "))
-m4=int(input("Enter your Marks for subject4: "))
-m5=int(input("Enter your Marks for subject5: "))
+m1 = get_marks("subject1")
+m2 = get_marks("subject2")
+m3 = get_marks("subject3")
+m4 = get_marks("subject4")
+m5 = get_marks("subject5")
 
-Total= m1+m2+m3+m4+m5
-percentage =(Total/500)*100
+Total = m1 + m2 + m3 + m4 + m5
+percentage = (Total / 500) * 100
 
 if percentage >= 90:
     grade = 'A'
@@ -21,8 +28,7 @@ elif percentage >= 60:
 else:
     grade = 'F'
 
-
-print("n\ Student Name: ",a)
-print("Total Marks: ", Total)
-print("percentage: ",percentage)
-print("Grade:",grade)
+print("\nStudent Name:", a)
+print("Total Marks:", Total)
+print("Percentage:", percentage)
+print("Grade:", grade)
